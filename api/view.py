4c79ad34dd63408f4e59ee5723e20e540a7477f4
Marie infantraj <marioredfoxfshm@gmail.com>
2019-12-03 15:47:29 +0530

    wrote the test case and working fine, test case for comparing the response and payload

--- api/view.py
+++ api/view.py
@@ -9,31 +9,31 @@ from schema import user_schema, users_schema
 def add_user():
     username = request.json['username']
     email = request.json['email']
-
+    
     new_user = User(username, email)
 
     db.session.add(new_user)
     db.session.commit()
 
-    return jsonify(new_user)
+    return user_schema.dump(new_user)
 
 
-# endpoint to show all users
-@app.route("/users", methods=["GET"])
+#show all users
+@app.route("/user", methods=["GET"])
 def get_user():
     all_users = User.query.all()
     result = users_schema.dump(all_users)
     return jsonify(result)
 
 
-# endpoint to get user detail by id
+#get user detail by id
 @app.route("/user/<id>", methods=["GET"])
 def user_detail(id):
     user = User.query.get(id)
     return user_schema.jsonify(user)
 
 
-# endpoint to update user
+#update user
 @app.route("/user/<id>", methods=["PUT"])
 def user_update(id):
     user = User.query.get(id)
